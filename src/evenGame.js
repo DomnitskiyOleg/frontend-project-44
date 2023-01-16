564881734697b09b195e263a06e0905874e06d49
Oleg Domnitskiy <domnitskiy.oleg@mail.ru>
2023-01-16 19:52:44 +0600

    Refactoring code in evenGame.js

--- src/evenGame.js
+++ src/evenGame.js
@@ -2,35 +2,44 @@ import readlineSync from 'readline-sync';
 import findOutPlayerName from './cli.js';
 
 const name = findOutPlayerName();
+const randomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+const checkIsNumberEven = (number) => number % 2 === 0;
+const findingCorrectAnswer = (argument) => (argument === true ? 'yes' : 'no');
 const isNumberEven = () => {
+  let randomNumber = randomInteger(1, 20);
+  let correctAnswer = findingCorrectAnswer(checkIsNumberEven(randomNumber));
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  console.log('Question: 15');
+  console.log(`Question: ${randomNumber}`);
   let playerAnswer = readlineSync.question('Your answer: ');
-  if (playerAnswer === 'no') {
+  if (playerAnswer === correctAnswer) {
     console.log('Correct!');
   } else {
     return console.log(
-      `'${playerAnswer}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}`,
+      `'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}`,
     );
   }
-  console.log('Question: 6');
+  randomNumber = randomInteger(1, 20);
+  correctAnswer = findingCorrectAnswer(checkIsNumberEven(randomNumber));
+  console.log(`Question: ${randomNumber}`);
   playerAnswer = readlineSync.question('Your answer: ');
-  if (playerAnswer === 'yes') {
+  if (playerAnswer === correctAnswer) {
     console.log('Correct!');
   } else {
     return console.log(
-      `'${playerAnswer}' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${name}`,
+      `'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}`,
     );
   }
-  console.log('Question: 7');
+  randomNumber = randomInteger(1, 20);
+  correctAnswer = findingCorrectAnswer(checkIsNumberEven(randomNumber));
+  console.log(`Question: ${randomNumber}`);
   playerAnswer = readlineSync.question('Your answer: ');
-  if (playerAnswer === 'no') {
+  if (playerAnswer === correctAnswer) {
     console.log('Correct!');
   } else {
     return console.log(
-      `'${playerAnswer}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}`,
+      `'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}`,
     );
   }
-  return console.log(`Congratulations, ${name}!`);
+  return console.log(`Congratulations, ${name}`);
 };
 export default isNumberEven;
