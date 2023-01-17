15f1d7d90e9183d34c641b51f8b62c238a120187
Oleg Domnitskiy <domnitskiy.oleg@mail.ru>
2023-01-17 15:44:06 +0600

    Refactoring code

--- src/evenGame.js
+++ src/evenGame.js
@@ -1,45 +1,22 @@
-import readlineSync from 'readline-sync';
+import inputOutputOperation from './inputOutputLogic.js';
 import findOutPlayerName from './cli.js';
 
-const name = findOutPlayerName();
-const randomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-const checkIsNumberEven = (number) => number % 2 === 0;
-const findingCorrectAnswer = (argument) => (argument === true ? 'yes' : 'no');
 const isNumberEven = () => {
-  let randomNumber = randomInteger(1, 20);
-  let correctAnswer = findingCorrectAnswer(checkIsNumberEven(randomNumber));
+  const name = findOutPlayerName();
+  const randomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+  const checkIsNumberEven = (number) => number % 2 === 0;
+  const findingCorrectAnswer = (argument) => (argument === true ? 'yes' : 'no');
+  let randomNumber;
+  let correctAnswer;
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  console.log(`Question: ${randomNumber}`);
-  let playerAnswer = readlineSync.question('Your answer: ');
-  if (playerAnswer === correctAnswer) {
-    console.log('Correct!');
-  } else {
-    return console.log(
-      `'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}`,
-    );
+  for (let i = 0; i <= 2; i += 1) {
+    randomNumber = randomInteger(1, 20);
+    correctAnswer = findingCorrectAnswer(checkIsNumberEven(randomNumber));
+    const result = inputOutputOperation(randomNumber, correctAnswer, name);
+    if (result === 'wrong') {
+      return false;
+    }
   }
-  randomNumber = randomInteger(1, 20);
-  correctAnswer = findingCorrectAnswer(checkIsNumberEven(randomNumber));
-  console.log(`Question: ${randomNumber}`);
-  playerAnswer = readlineSync.question('Your answer: ');
-  if (playerAnswer === correctAnswer) {
-    console.log('Correct!');
-  } else {
-    return console.log(
-      `'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}`,
-    );
-  }
-  randomNumber = randomInteger(1, 20);
-  correctAnswer = findingCorrectAnswer(checkIsNumberEven(randomNumber));
-  console.log(`Question: ${randomNumber}`);
-  playerAnswer = readlineSync.question('Your answer: ');
-  if (playerAnswer === correctAnswer) {
-    console.log('Correct!');
-  } else {
-    return console.log(
-      `'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}`,
-    );
-  }
-  return console.log(`Congratulations, ${name}!`);
+  return console.log(`Congratultaions, ${name}!`);
 };
 export default isNumberEven;
