70ed52093c44d48f543d40f92d75fda762c2f3c2
Oleg Domnitskiy <domnitskiy.oleg@mail.ru>
2023-01-18 17:49:21 +0600

    Refactoring code

--- src/evenGame.js
+++ src/evenGame.js
@@ -1,22 +1,13 @@
-import inputOutputOperation from './inputOutputLogic.js';
-import findOutPlayerName from './cli.js';
+import gameEngine from './index.js';
+import { randomInteger, checkIsNumberEven } from './operationsWithNumbers.js';
 
 const isNumberEven = () => {
-  const name = findOutPlayerName();
-  const randomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-  const checkIsNumberEven = (number) => number % 2 === 0;
   const findingCorrectAnswer = (argument) => (argument === true ? 'yes' : 'no');
-  let randomNumber;
-  let correctAnswer;
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  for (let i = 0; i <= 2; i += 1) {
-    randomNumber = randomInteger(1, 20);
-    correctAnswer = findingCorrectAnswer(checkIsNumberEven(randomNumber));
-    const result = inputOutputOperation(randomNumber, correctAnswer, name);
-    if (result === 'wrong') {
-      return false;
-    }
-  }
-  return console.log(`Congratultaions, ${name}!`);
+  const gameQuestion = 'Answer "yes" if the number is even, otherwise answer "no".';
+  const randomNumber = randomInteger(1, 10);
+  const correctAnswer = findingCorrectAnswer(checkIsNumberEven(randomNumber));
+  const questionForPlayer = `${randomNumber}`;
+  return [gameQuestion, questionForPlayer, correctAnswer];
 };
+gameEngine(isNumberEven);
 export default isNumberEven;
